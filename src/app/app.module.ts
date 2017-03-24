2a3dc47a21e7a4d1cff391e33b76801a32287e2f
Miguel Martínez <miguelmc.18@hotmail.com>
2017-03-23 21:02:01 -0600

    Firebase link

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,37 @@ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { AngularFireModule } from 'angularfire2';
+import { DetailPage } from '../pages/detail/detail';
+import { AdoptPage } from '../pages/adopt/adopt';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCKq0Gz2ltLYc_RKJusJij2c0R2GaxU2y8",
+  authDomain: "dm-pet.firebaseapp.com",
+  databaseURL: "https://dm-pet.firebaseio.com",
+  storageBucket: "dm-pet.appspot.com",
+  messagingSenderId: "137998444426"
+};
 
 @NgModule({
   declarations: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    DetailPage,
+    AdoptPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     Page1,
-    Page2
+    Page2,
+    DetailPage,
+    AdoptPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
--- src/pages/cards/pages.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from '@angular/core';
-
-
-@Component({
-  templateUrl: 'pages.html'
-})
-export class ImagePage { }
